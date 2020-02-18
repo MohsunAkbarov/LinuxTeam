4ba5fa5ab5201cfcde250f63b25b309aeb2417ca
MohsunAkbarov <61051386+MohsunAkbarov@users.noreply.github.com>
2020-02-18 21:44:51 +0400

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,11 @@
+$databases = [];
+$databases['default']['default'] = [
+     'database' => 'drupal',
+     'username' => 'drupal-user',
+     'password' => '12345',
+     'host' => 'localhost',
+     'port' => '3306',
+     'driver' => 'mysql',
+     'prefix' => '',
+     'collation' => 'utf8mb4_general_ci',
+  ];
