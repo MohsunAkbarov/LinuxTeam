c30b579f2d4f8e2085fdb462a02ded0225e6db24
MohsunAkbarov <61051386+MohsunAkbarov@users.noreply.github.com>
2020-02-19 21:34:32 +0400

    Update settings.php

--- settings.php
+++ settings.php
@@ -755,7 +755,6 @@ $settings['entity_update_backup'] = TRUE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases = [];
 $databases['default']['default'] = [
      'database' => 'drupal',
      'username' => 'drupal-user',
